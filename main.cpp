0872ba1aaa19f3f185f2858e8c25a1870582836b
darius <T1ckleT@ckle>
2020-01-29 11:34:05 -0800

    added two integers and an output message

--- main.cpp
+++ main.cpp
@@ -5,7 +5,11 @@ using std::cout;
 
 int main()
 {
+    int num1;
+    int num2;
     cout<<"Hi, please enter two numbers: ";
-
+    cin>>num1;
+    cin>>num2;
+    cout<<"You entered: " + num1 + "and " + num2;
     return 0;
 }
