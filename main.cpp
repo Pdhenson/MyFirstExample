5c83baa28ae2915d31ed7332ef5fe5c6c89278a4
darius <T1ckleT@ckle>
2020-03-11 11:31:33 -0700

    added new function with errors

--- main.cpp
+++ main.cpp
@@ -4,13 +4,21 @@ using std::cin;
 using std::cout;
 using std::endl;
 
+int temp(int);
+
 int main()
 {
-    int x,y;
+    int x,y,z;
     
+    temp(z);
+
     cout<<"Hi, please enter two numbers: ";
     cin >> x >> y;
     cout << x + y << endl;
     
     return 0;
+}
+
+int temp(int num){
+return num;
 }
